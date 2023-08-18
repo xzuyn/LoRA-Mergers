51ea17a650c3909a2e87f5e3d99056c145debd6f
xzuyn <16216325+xzuyn@users.noreply.github.com>
2023-08-18 17:34:02 -0400

    Update simple_adapter_merge_safetensors.py

--- simple_adapter_merge_safetensors.py
+++ simple_adapter_merge_safetensors.py
@@ -6,6 +6,7 @@ from safetensors.torch import save_file
 def Blend(aPath, bPath, outName):
     a = {}
     b = {}
+    only_copy = True
     with safe_open(aPath, framework="pt", device="cpu") as fa:
         for ka in fa.keys():
             a[ka] = fa.get_tensor(ka)
@@ -15,17 +16,23 @@ def Blend(aPath, bPath, outName):
     
     all_keys = list(set(a.keys()) | set(b.keys()))  # Get all unique keys
     
-    for key in tqdm(all_keys):
+    for key in all_keys:
         if key in a and key in b:
+            print(f"Merging tensors for key: {key}, Size: {a[key].size()}")
             a[key] = (a[key] + b[key]) / 2
+            only_copy = False
         elif key in b:
+            print(f"Copying tensor for key: {key}, Size: {b[key].size()}")
             a[key] = b[key]
         # No need to handle the case where key is only in 'a', as it is already in 'a'
 
-    save_file(a, outName)
+    if only_copy is True:
+       print("No merge happened, only copies. Not saving.")
+    elif only_copy is False:
+        save_file(a, outName)
 
 Blend(
-    "Gloomifier_V2_TheGlow.safetensors",
-    "Gloomifier_TheDread_V1_LECO.safetensors",
-    "GlowingDread.safetensors"
+    ".safetensors",
+    ".safetensors",
+    ".safetensors"
 )
